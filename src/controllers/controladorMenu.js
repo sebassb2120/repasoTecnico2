4db0abe549a30682545d228454431b2b19a6ca1e
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-03-10 11:33:35 -0500

    SE CREA PROYECTO DE REPASO

---
+++ src/controllers/controladorMenu.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("menu")
+
+window.addEventListener("scroll",function(){
+
+    if(document.documentElement.scrollTop>100){
+        menu.classList.add("menuSecundario")
+        menu.classList.remove("menuPrincipal")
+
+    }else{
+        menu.classList.add("menuPrincipal")
+        menu.classList.remove("menuSecundario")
+    }
+
+})
